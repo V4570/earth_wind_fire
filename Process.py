e15a6581cb9a2aa12081a2d82829413dce539596
george <george030796@gmail.com>
2019-11-30 19:34:08 +0200

    minor changes

--- Process.py
+++ Process.py
@@ -1,11 +1,31 @@
+import numpy as np 
 
 
 
 
 
-
-class Node():
+class Process():
 	def __init__(self):
+		print (" Do your magic things")
+
+	def dataProcess(self):
 
 
-	
+		return None
+
+
+	def _energyWind(self, p, u):
+				"""
+			Cp = 
+			p = air densirty 
+			u = air speed
+		"""
+		return 0,5*p*(u^3)
+
+	def _energyWaves(self, Cp, p, u):
+		"""
+			Cp = 
+			p = water densirty 
+			u = waves speed
+		"""
+		return 0,5*Cp*p*(u^3)
