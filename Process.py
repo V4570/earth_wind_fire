082f5c246e3386a31ff834c3bb92edbce386dfdd
CBnareli <50832195+CBnareli@users.noreply.github.com>
2019-12-01 02:42:46 +0200

    Update Process.py
    dogas data parser

--- Process.py
+++ Process.py
@@ -31,25 +31,26 @@ class Process():
 		return 0,5*Cp*p*(u^3)
 
 	def smthng(s):
-		min1=s[0][0]
-		max1=s[0][0]
-		min2=s[0][1]
-		max2=s[0][1]
-		for i in s:
-			if i[0]>max1:
-				max1=i[0]
-			elif i[0]<min1:
-				min1=i[0]
+	    min1=s[0][0]
+	    max1=s[0][0]
+	    min2=s[0][1]
+	    max2=s[0][1]
+	    for i in s:
+	        if i[0]>max1:
+	            max1=i[0]
+	        elif i[0]<min1:
+	            min1=i[0]
 
-			if i[1]>max2:
-				max2=i[1]
-			elif i[1]<min2:
-				min2=i[1]
-		x=max1-min1
-		y=max2-min2
-		teliko=[]
-		for i in range(x):
-			for j in range(y):
-				teliko.append([0,0])
-		for i in s:
-				teliko[i[0]-x][i[1]-y].append(i)
+	        if i[1]>max2:
+	            max2=i[1]
+	        elif i[1]<min2:
+	            min2=i[1]
+	    x=max1-min1
+	    y=max2-min2
+	    teliko=[]
+	    for i in range(x):
+	        for j in range(y):
+	            teliko.append([[0,0]])
+	    for w in s:
+	            teliko[w[0]-min1][w[1]-min2].append(w)
+	    return teliko
